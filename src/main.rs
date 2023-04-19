8fc72acac54b4dfe8b32eef39bae5b39230dee61
jlchamba_escritorio <jlchamba2986@outlook.com>
2023-04-18 20:10:25 -0500

    Comenzando con Rust

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+
+    const PI: f32 = 3.1416;
+    let x = 3;
+    //let char y = 'a';
+    println!("El valor de x es: {}", x);
+
+   
+    //println!("El valor de x es: {}", y);
+    println!("El valor de PI es: {}", PI);
+}
