9430112e8462dc3b7de049c1307dd918d6437374
jlchamba_escritorio <jlchamba2986@outlook.com>
2024-08-17 21:26:57 -0500

    Actualizando el archivo rust main

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,26 @@ fn main() {
     const PI: f32 = 3.1416;
     let x = 3;
     let c = 'a';
+    let y: f64 = 2.5;
+    let z: f64 = 1.5;
+    let resultado: f64 = y/z;
     println!("El valor de x es: {}", x);
+    println!("El valor de la división es: {}", resultado);
 
     //let mult = PI * 5;
 
    
     println!("El valor de Y es: {}", c);
     println!("El valor de PI es: {}", PI);
-}
 
-
-fn main(){
-    let a = [1, 2, 3, 4, 5];
+    let a = [1.5, 2.7, 3.6, 4.3, 5.7];
+    let b = [1, 2, 3, 4, 5];
 
     let primero = a[0];
     let tercero = a[2];
 
-    print!("El primer elemento de la lista es: {primero}");
+    print!("El primer elemento de la lista es: {primero}\n");
     print!("El tercer elemento de la lista es: {tercero}");
-}
+}//
+
+
